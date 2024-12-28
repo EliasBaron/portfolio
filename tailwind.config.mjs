a7b0bc97c4cf1e508639977d21bab166cce2c7b8
eliasBaron <eliasnbaron@gmail.com>
2024-12-28 02:25:53 -0300

    fixed background gradient

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+import tailwindcssAnimated from 'tailwindcss-animated';
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkmode: 'class', 
-	theme: {
-		extend: {},
-	},
-  plugins: [require('tailwindcss-animated')],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', 
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'radial-gradient(ellipse 80% 80% at 50% -20%, rgba(120,119,198,0.3), rgba(255,255,255,0))',
+      },
+    },
+  },
+  plugins: [tailwindcssAnimated],
+};
